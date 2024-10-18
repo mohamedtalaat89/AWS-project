8c231060adf9a02a24f7aca4d88cd5fb3372a46c
mohamedtalaat89 <engmohamedtalaat89@gmail.com>
2024-10-18 22:29:22 +0300

    Create sg.tf

---
+++ Project3/sg.tf
@@ -0,0 +1,53 @@
+# Security Groups
+resource "aws_security_group" "HTTP-SG" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "HTTP-SG"
+  }
+}
+
+resource "aws_security_group" "bastion_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "bastion-sg"
+    Environment = var.Environment
+    Owner       = var.Owner
+  }
+}
